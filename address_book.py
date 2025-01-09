ce3dd9903c859349f183a6513944e9d5c8d4b84f
oleHxOleksuk <108345191+oleHxOleksuk@users.noreply.github.com>
2025-01-09 20:04:21 +0200

    wewrt

--- address_book.py
+++ address_book.py
@@ -1,42 +1,54 @@
-from collections import UserDict
 from datetime import datetime, timedelta
+from collections import UserDict
+from const import DATE_FORMAT
+
+
+def is_weekend_day(day: int) -> bool:
+    return day > 4
+
 
 class AddressBook(UserDict):
-  
-  def add_record(self, record):
-    if record.name.value == self.data:
-        raise KeyError(f"Record with name '{record.name.value}' already exists.")
-    self.data[record.name.value] = record
 
-  def find(self, name):
-     record = self.data.get(name, None)
-     if record is None:
-        raise KeyError(f"Record with name '{name}' not found.")
-     return record
-  
-  def delete(self, name):
-     if name not in self.data:
-        raise KeyError(f"Record with name '{name}' not found.")
-     del self.data[name]
+    def __str__(self):
+        lines = [str(record) for record in self.data.values()]
+        return "\n".join(lines)
 
-  def get_upcoming_birthdays(self):
-     today_date = datetime.today().date()
-     upcoming_birthdays = []
+    def add_record(self, record):
+        if record.name.value in self.data:
+            raise KeyError(f"Record with name '{record.name.value}' already exists.")
+        self.data[record.name.value] = record
 
-     for name, record in self.data.items():
-        if record.birthday:
-            birthday_date = record.birthday.value.replace( year=today_date.year).date()
-            timedelta_days = (birthday_date - today_date).days
-            if 0 <= timedelta_days <= 7:
-               if birthday_date.weekday() > 4:
-                  days_delta = 2 if birthday_date.weekday() == 5 else 1
-                  congratulation_date = birthday_date + timedelta(days=days_delta)
-               else:
-                  congratulation_date = birthday_date
-                  upcoming_birthdays.append(
+    def find(self, name: str):
+        return self.data.get(name)
+
+    def delete(self, name):
+        del self.data[name]
+
+    def get_upcoming_birthdays(self):
+        today_date = datetime.today().date()
+        upcoming_birthdays = []
+
+        for name, record in self.data.items():
+            if record.birthday:
+                birthday_date = record.birthday.value.replace(
+                    year=today_date.year
+                ).date()
+                timedelta_days = (birthday_date - today_date).days
+
+                if 0 <= timedelta_days <= 7:
+                    if is_weekend_day(birthday_date.weekday()):
+                        days_delta = 2 if birthday_date.weekday() == 5 else 1
+                        congratulation_date = birthday_date + timedelta(days=days_delta)
+                    else:
+                        congratulation_date = birthday_date
+
+                    upcoming_birthdays.append(
                         {
                             "name": name,
-                            "congratulation_date": congratulation_date.strftime("%d.%m.%Y"),
+                            "congratulation_date": congratulation_date.strftime(
+                                DATE_FORMAT
+                            ),
                         }
                     )
-     return upcoming_birthdays
+
+        return upcoming_birthdays
--- main.py
+++ main.py
@@ -80,7 +80,6 @@ def show_birthday(args, book: AddressBook):
     else:
         return not_found_message
 
-
 def parse_input(user_input):
     cmd, *args = user_input.split()
     cmd = cmd.strip().lower()
